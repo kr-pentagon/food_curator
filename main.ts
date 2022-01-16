19fed189768cb2611295a28e93d8ec9de5f79ec6
kr-pentagon <96647530+kr-pentagon@users.noreply.github.com>
2022-01-16 15:27:58 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
-basic.forever(function () {
-	
-})
+let 온도 = 0
+while (true) {
+    if (온도 != input.temperature()) {
+        if (input.temperature() < -3) {
+            basic.showString("")
+        } else if (input.temperature() >= -3 && input.temperature() <= 0) {
+            basic.showString("Hotteok")
+        } else if (input.temperature() <= 10) {
+            basic.showString("Kimbap")
+        } else if (input.temperature() <= 20) {
+            basic.showString("Macdonald")
+        } else if (input.temperature() <= 30) {
+            basic.showString("Dukbokki")
+        } else if (input.temperature() <= 35) {
+            basic.showString("icecream")
+        } else {
+            basic.showString("DO NOT EAT")
+        }
+        온도 = input.temperature()
+    }
+}
