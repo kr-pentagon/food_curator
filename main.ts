99bd98575a9601e5bbaed76e60718c14f6687bbd
kr-pentagon <96647530+kr-pentagon@users.noreply.github.com>
2022-01-16 15:59:49 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,21 +1,24 @@
+function recomendFood (num: number) {
+    if (num < -3) {
+        basic.showString("")
+    } else if (num >= -3 && num <= 0) {
+        basic.showString("Hotteok")
+    } else if (num <= 10) {
+        basic.showString("Kimbap")
+    } else if (num <= 20) {
+        basic.showString("Macdonald")
+    } else if (num <= 30) {
+        basic.showString("Dukbokki")
+    } else if (num <= 35) {
+        basic.showString("icecream")
+    } else {
+        basic.showString("DO NOT EAT")
+    }
+}
 let 온도 = 0
 while (true) {
     if (온도 != input.temperature()) {
-        if (input.temperature() < -3) {
-            basic.showString("")
-        } else if (input.temperature() >= -3 && input.temperature() <= 0) {
-            basic.showString("Hotteok")
-        } else if (input.temperature() <= 10) {
-            basic.showString("Kimbap")
-        } else if (input.temperature() <= 20) {
-            basic.showString("Macdonald")
-        } else if (input.temperature() <= 30) {
-            basic.showString("Dukbokki")
-        } else if (input.temperature() <= 35) {
-            basic.showString("icecream")
-        } else {
-            basic.showString("DO NOT EAT")
-        }
-        온도 = input.temperature()
+        recomendFood(input.temperature())
     }
+    온도 = input.temperature()
 }
